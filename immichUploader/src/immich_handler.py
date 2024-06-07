9eeffb9966dd393e575dbff60314aebca0843ff8
Dhairyashil V Patil <dh.patil09@gmail.com>
2024-06-06 20:02:34 -0700

    modified code to add error handling

--- immichUploader/src/immich_handler.py
+++ immichUploader/src/immich_handler.py
@@ -1,16 +1,13 @@
 # src/immich_handler.py
 import os
 import subprocess
+import shutil
 from tqdm import tqdm
-from utils import get_timestamp, log_message
+from utils import get_timestamp, log_message, is_folder_empty, immich_login, get_log_file
 from dotenv import load_dotenv
 
 def run_immich_command(folder, log_file):
     try:
-        load_dotenv()  # Load environment variables from .env file
-        api_url = os.getenv("API_URL")
-        token = os.getenv("TOKEN")
-        
         immich_path = shutil.which("immich")
         if immich_path is None:
             raise FileNotFoundError("The 'immich' command is not found in the system PATH.")
@@ -25,3 +22,23 @@ def run_immich_command(folder, log_file):
             log_message(line.strip(), log_file)
     except Exception as e:
         log_message(f"Error occurred while processing folder '{folder}': {str(e)}", log_file)
+
+def process_folders(root_dir):
+    log_file = get_log_file("upload_log")
+    err_log = get_log_file("error_log")
+    
+    try:
+        load_dotenv()  # Load environment variables from .env file
+        api_url = os.getenv("API_URL")
+        token = os.getenv("TOKEN")
+        log_message(f"API env details fetched API URL:{api_url} {token}",log_file)
+        immich_login(api_url, token, log_file)
+    except Exception as e:
+        log_message(f"Error occurred during login: {str(e)}", err_log)
+        return
+    
+    for root, dirs, _ in os.walk(root_dir, topdown=False):  # Process nested subdirectories first
+        for dir_name in dirs:
+            folder_path = os.path.join(root, dir_name)
+            if not is_folder_empty(folder_path, err_log):
+                run_immich_command(folder_path, log_file)
--- immichUploader/src/main.py
+++ immichUploader/src/main.py
@@ -1,10 +1,9 @@
 # src/main.py
 import os
-from immich_handler import process_folders
+from immich_handler import run_immich_command,process_folders
 from dotenv import load_dotenv
 
 def main():
-    load_dotenv()  # Load environment variables from .env file
     root_directory = "E:\\Photos"  # Example directory
     process_folders(root_directory)
 
